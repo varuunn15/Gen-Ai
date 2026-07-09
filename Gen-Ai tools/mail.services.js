3232b26e236711ef3dbd78cd19edefcb42638ff4
varunO6 <varunvisoriya@gmail.com>
2026-07-09 11:54:07 +0530

    gen ai tools

---
+++ Gen-Ai tools/mail.services.js
@@ -0,0 +1,31 @@
+import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+dotenv.config();
+
+const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+        user: process.env.GOOGLE_USER,
+        pass: process.env.GOOGLE_APP_PASSWORD
+    }
+})
+
+transporter.verify()
+    .then(() => { console.log("Email transporter is ready to send emails"); })
+    .catch((err) => { console.error("Email transporter verification failed:", err); });
+
+
+export async function sendEmail({ to, subject, html, text="" }) {
+
+    const mailOptions = {
+        from: process.env.GOOGLE_USER,
+        to,
+        subject,
+        html,
+        text
+    };
+
+    const details = await transporter.sendMail(mailOptions);
+    console.log("Email sent:", details);
+    return "email sent successfully, to " + to;
+}
