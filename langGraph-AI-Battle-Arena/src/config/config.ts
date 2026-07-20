0810952bfae19c09863314de94bffa2885b8c435
varunO6 <varunvisoriya@gmail.com>
2026-07-20 16:07:24 +0530

    langGraph foundation

---
+++ langGraph-AI-Battle-Arena/src/config/config.ts
@@ -0,0 +1,13 @@
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const config = {
+    GOOGLE_API_KEY: process.env.GOOGLE_API_KEY || '',
+    MISTRAL_API_KEY: process.env.MISTRAL_API_KEY || '',
+    COHERE_API_KEY: process.env.COHERE_API_KEY || '',
+}
+
+
+export default config;
