b53b970f1d870356ede027d9a216d57da8469ed4
varunO6 <varunvisoriya@gmail.com>
2026-07-21 13:32:59 +0530

    frontend integration

--- Gen-Ai/index.js
+++ Gen-Ai/index.js
@@ -2,22 +2,7 @@ import "dotenv/config";
 import readline from "readline/promises";
 import { ChatMistralAI } from "@langchain/mistralai";
 import { HumanMessage } from "@langchain/core/messages";
-// import { sendEmail } from "./mail.service.js";
-// import * as z from "zod";
 
-
-// const emailTool = tool(
-//     sendEmail,
-//     {
-//         name: "emailTool",
-//         description: "Use this tool to send an email",
-//         schema: z.object({
-//             to: z.string().describe("The recipient's email address"),
-//             html: z.string().describe("The HTML content of the email"),
-//             subject: z.string().describe("The subject of the email"),
-//         })
-//     }
-// )
 
 
 // ye terminal se input read and write k liye
@@ -31,10 +16,7 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest",
 });
 
-// const agent = createAgent({
-//     model,
-//     tools: [ emailTool ]
-// })
+
 
 // conversation history maintain karne ke liye
 const messages = [];
